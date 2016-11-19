2790a8701252a6cc453e9eb70dcbfd3281d4fdee
Donni Richasdy <donni.richasdy@gmail.com>
2016-11-19 20:34:35 +0700

    card domain

---
+++ src/main/java/com/richasdy/presencesys/domain/Card.java
@@ -0,0 +1,122 @@
+package com.richasdy.presencesys.domain;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+@Entity
+public class Card {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(unique = true, nullable = false)
+	private int id;
+
+	@Column(unique = true, nullable = false)
+	private String cardNumber;
+
+	private Boolean activated;
+
+	private String note;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date createdAt;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date updatedAt;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date deletedAt;
+
+	public Card() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Card(int id, String cardNumber, Boolean activated, String note, Date createdAt, Date updatedAt,
+			Date deletedAt) {
+		super();
+		this.id = id;
+		this.cardNumber = cardNumber;
+		this.activated = activated;
+		this.note = note;
+		this.createdAt = createdAt;
+		this.updatedAt = updatedAt;
+		this.deletedAt = deletedAt;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCardNumber() {
+		return cardNumber;
+	}
+
+	public void setCardNumber(String cardNumber) {
+		this.cardNumber = cardNumber;
+	}
+
+	public Boolean getActivated() {
+		return activated;
+	}
+
+	public void setActivated(Boolean activated) {
+		this.activated = activated;
+	}
+
+	public String getNote() {
+		return note;
+	}
+
+	public void setNote(String note) {
+		this.note = note;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	public Date getDeletedAt() {
+		return deletedAt;
+	}
+
+	public void setDeletedAt(Date deletedAt) {
+		this.deletedAt = deletedAt;
+	}
+
+	@Override
+	public String toString() {
+		return this.getClass().getName() + " [ " + id + ", " + cardNumber + ", " + activated + ", " + note + ", "
+				+ createdAt + ", " + updatedAt + ", " + deletedAt + " ]";
+	}
+
+}
