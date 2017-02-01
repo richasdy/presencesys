53412d5cc04caafd2048b26c2b5897793223e1c7
Donni Richasdy <donni.richasdy@gmail.com>
2017-02-01 16:22:43 +0700

    tapping controller refinement, error build in heroku, remove unused import

--- src/main/java/com/richasdy/presencesys/tap/TappingController.java
+++ src/main/java/com/richasdy/presencesys/tap/TappingController.java
@@ -1,27 +1,12 @@
 package com.richasdy.presencesys.tap;
 
 import java.util.Date;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
 
-import org.assertj.core.util.Lists;
-import org.springframework.beans.BeanUtils;
-import org.springframework.beans.BeanWrapper;
-import org.springframework.beans.BeanWrapperImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
