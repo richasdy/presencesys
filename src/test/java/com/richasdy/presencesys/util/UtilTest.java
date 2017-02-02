5d42b4ba9cffa122e3320ccb539fc809aef4bb20
Donni Richasdy <donni.richasdy@gmail.com>
2017-02-02 11:27:34 +0700

    hello java Date

--- src/test/java/com/richasdy/presencesys/util/UtilTest.java
+++ src/test/java/com/richasdy/presencesys/util/UtilTest.java
@@ -9,6 +9,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.TimeZone;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.Test;
 
@@ -62,6 +63,46 @@ public class UtilTest {
 		System.out.println(TimeZone.getDefault());
 		// Date valid = Util.stringToDate("2016-11-s20");
 		// assertTrue("failure - expected null", valid == null);
+
+	}
+
+	// @Test
+	public void datetry() throws ParseException {
+
+		Date sekarang = new Date();
+
+		try {
+			TimeUnit.SECONDS.sleep(3);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		Date setelah = new Date();
+
+		System.out.println("COMPARE");
+		System.out.println("sekarang : " + sekarang);
+		System.out.println("setelah : " + setelah);
+		System.out.println("operation : " + setelah.compareTo(sekarang));
+		System.out.println("operation : " + sekarang.compareTo(setelah));
+
+		System.out.println("DATE FORMAT");
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		Date parsingTanggal = format.parse("2016-01-13");
+		System.out.println("sekarang : " + format.format(sekarang));
+		System.out.println("parsingTanggal : " + parsingTanggal);
+		System.out.println("parsingTanggal : " + format.format(parsingTanggal));
+
+		System.out.println("COMPARE WITH DATE FROMAT");
+		Date parsingTanggalsekarang = format.parse("2017-01-31");
+		System.out.println(parsingTanggalsekarang);
+		System.out.println(sekarang);
+		System.out.println("parsing");
+		System.out.println(format.format(parsingTanggalsekarang));
+		System.out.println(format.format(sekarang));
+
+		System.out.println(parsingTanggalsekarang.equals(format.format(sekarang)));
+		System.out.println(format.format(parsingTanggalsekarang).equals(format.format(sekarang)));
 
 	}
 
